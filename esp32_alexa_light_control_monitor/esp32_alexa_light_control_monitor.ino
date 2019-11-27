d7e7305f2b7f1f23ba637f81e1d6f8cb86147b9a
daiki <daiking_tensai@yahoo.co.jp>
2019-11-27 16:05:27 +0900

    support voice command & display command

--- esp32_alexa_light_control_monitor/esp32_alexa_light_control_monitor.ino
+++ esp32_alexa_light_control_monitor/esp32_alexa_light_control_monitor.ino
@@ -10,11 +10,16 @@
 //    Upload Speed  115200
 //    Core Debug Level なし
 
+#include <Arduino.h>
+#include <WiFi.h>
+
 #include <IRremoteESP8266.h>
 #include <IRsend.h>
 
 #include <Ticker.h>
 #include <SPI.h>
+
+#include "fauxmoESP.h"
 
 // TFT display module
 #include <gfxfont.h>
@@ -23,6 +28,8 @@
 #include <Adafruit_SPITFT_Macros.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_ILI9341.h>
+
+#define LIGHT_PIN 19
 
 #define TFT_CS   13
 #define TFT_DC   14
@@ -82,89 +89,175 @@ void stopTimer() {
 }
 
 
-IRsend irsend(19);
+typedef enum {
+    NO_EVENT    = 0,
+    LIGHT_ON,
+    LIGHT_OFF
+} ALEXA_EVENT;
 
 struct AlexaInfo {
+  ALEXA_EVENT event;
   bool lightIsOn;
 };
 
-AlexaInfo alexa = {true};
+AlexaInfo alexa = {NO_EVENT, true};
 
-void setup() {
-  Serial.begin(115200);
+IRsend irsend(LIGHT_PIN);
 
-  initTouch();
+fauxmoESP fauxmo;
+
+char ssid[] = "Buffalo-G-05F0";
+char password[] = "kfnxi7wdn7d7w";
+
+volatile ALEXA_EVENT alexa_event = NO_EVENT;
+
+void WifiSetup()
+{
+  WiFi.mode(WIFI_STA);
+  Serial.printf("connecting to %s¥n", ssid);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(100);
+  }
+
   irsend.begin();
 
-  tft.begin();
-  tft.fillScreen(ILI9341_BLACK);
-  yield();
-  tft.setCursor(0,0);
-  tft.setTextColor(ILI9341_GREEN);
-  tft.setTextSize(2);
-  tft.setRotation(3);
+  Serial.println();
+  Serial.print("WiFi connected: ");
+  Serial.println(WiFi.localIP());
+}
 
-  pinMode(TFT_LED, OUTPUT);
-  digitalWrite(TFT_LED, HIGH); // back light on
+void setup() {
+    Serial.begin(115200);
 
-  tft.println("Start");
-  touch.timeoutCount = 0;
-  touch.isDetected = true;
-  startTimer();
+    WifiSetup();
+
+    fauxmo.createServer(true);
+    fauxmo.setPort(80);
+    fauxmo.enable(true);
+
+    fauxmo.addDevice("ライト");
+    
+    fauxmo.onSetState([](unsigned char device_id, const char *device_name, bool state, unsigned char value) {
+      //  If you have to do something more involved here set a flag and process it in your main loop
+      Serial.printf("Device #%d (%s) state: %s value: %d\n", device_id, device_name, state ? "ON" : "OFF", value);
+      if (strcmp(device_name, "ライト") == 0) {
+          if(state) {
+            alexa_event = LIGHT_ON;
+          }
+          else {
+            alexa_event = LIGHT_OFF;
+          }
+      }
+    });
+
+    tft.begin();
+    tft.fillScreen(ILI9341_BLACK);
+    yield();
+    tft.setCursor(0,0);
+    tft.setTextColor(ILI9341_GREEN);
+    tft.setTextSize(2);
+    tft.setRotation(3);
+  
+    pinMode(TFT_LED, OUTPUT);
+    digitalWrite(TFT_LED, HIGH); // back light on
+  
+    tft.println("Start");
+
+}
+
+void handleEvent() {
+    switch (alexa_event) {
+      case LIGHT_ON:
+        Serial.println("LIGHT ON");
+        irsend.sendNEC(0x1768877,32); // ON
+        delay(50);
+        irsend.sendNEC(0x1768877,32); // ON
+        delay(50);
+        irsend.sendNEC(0x1768877,32); // ON
+        delay(100);
+        tft.println("LIGHT ON");
+        alexa_event = NO_EVENT;
+        break;
+      case LIGHT_OFF:
+        Serial.println("LIGHT OFF");
+        irsend.sendNEC(0x17600FF,32); // OFF
+        delay(50);
+        irsend.sendNEC(0x17600FF,32); // OFF
+        delay(50);
+        irsend.sendNEC(0x17600FF,32); // OFF
+        delay(100);
+        tft.println("LIGHT OFF");
+        alexa_event = NO_EVENT;
+        break;
+      default:
+        break;
+    }
 }
 
 void loop() {
-  delay(50);
-  if (touch.isDetected) {
-    if (touch.timerIsStopped) {
-      tft.println("Touched & LED on");
-      digitalWrite(TFT_LED, HIGH); // back light on
-      touch.timeoutCount = 0;
-      startTimer();
-
-      if (alexa.lightIsOn) {
-        alexa.lightIsOn = false;
-        Serial.println("LIGHT OFF");
-        irsend.sendNEC(0x17600FF, 32); //OFF
-      }
-      else {
-        alexa.lightIsOn = true;
-        Serial.println("LIGHT ON");
-        irsend.sendNEC(0x1768877, 32); //ON 
-      }
-
-    }
-    else { // timer is stareted
-      if (touch.timeoutCount >= touch.TIMEOUT_MAX_COUNT) {
-        stopTimer();
-        tft.println("LED off");
-        delay(1000);
-        portENTER_CRITICAL_ISR(&intrMux);
-        touch.isDetected = false;
-        portEXIT_CRITICAL_ISR(&intrMux);
-        digitalWrite(TFT_LED, LOW); // back light off
-      }
-    }
-  }
+    handleEvent();
+    fauxmo.handle();
 }
 
-
-//#include <IRremoteESP8266.h>
-//#include <IRsend.h>
+//void setup() {
+//  Serial.begin(115200);
 //
-//IRsend irsend(19);
+//  initTouch();
+//  irsend.begin();
 //
-//void setup()
-//{
-//    Serial.begin(115200);
-//      irsend.begin();
+//  tft.begin();
+//  tft.fillScreen(ILI9341_BLACK);
+//  yield();
+//  tft.setCursor(0,0);
+//  tft.setTextColor(ILI9341_GREEN);
+//  tft.setTextSize(2);
+//  tft.setRotation(3);
+//
+//  pinMode(TFT_LED, OUTPUT);
+//  digitalWrite(TFT_LED, HIGH); // back light on
+//
+//  tft.println("Start");
+//  touch.timeoutCount = 0;
+//  touch.isDetected = true;
+//  startTimer();
 //}
 //
 //void loop() {
-//  Serial.println("LIGHT OFF");
-//	irsend.sendNEC(0x17600FF, 32); //OFF
-//	delay(5000); //5 second delay between each signal burst
-//  Serial.println("LIGHT ON");
-//  irsend.sendNEC(0x1768877, 32); //ON 
-//  delay(5000); //5 second delay between each signal burst
+//  delay(50);
+//  if (touch.isDetected) {
+//    if (touch.timerIsStopped) {
+//      tft.println("Touched & LED on");
+//      digitalWrite(TFT_LED, HIGH); // back light on
+//      touch.timeoutCount = 0;
+//      startTimer();
+//
+//      if (alexa.lightIsOn) {
+//        alexa.lightIsOn = false;
+//        Serial.println("LIGHT OFF");
+//        irsend.sendNEC(0x17600FF, 32); //OFF
+//      }
+//      else {
+//        alexa.lightIsOn = true;
+//        Serial.println("LIGHT ON");
+//        irsend.sendNEC(0x1768877, 32); //ON 
+//      }
+//
+//    }
+//    else { // timer is stareted
+//      if (touch.timeoutCount >= touch.TIMEOUT_MAX_COUNT) {
+//        stopTimer();
+//        tft.println("LED off");
+//        delay(1000);
+//        portENTER_CRITICAL_ISR(&intrMux);
+//        touch.isDetected = false;
+//        portEXIT_CRITICAL_ISR(&intrMux);
+//        digitalWrite(TFT_LED, LOW); // back light off
+//      }
+//    }
+//  }
 //}
+
